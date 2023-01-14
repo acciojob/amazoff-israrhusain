a66c93bde3437e6f52e3d8e4708e066816badd64
israr <israrhusain5892@gmail.com>
2023-01-14 10:46:17 +0530

    hi

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,15 +1,12 @@
 package com.driver;
-
 public class Order {
 
     private String id;
     private int deliveryTime;
-    public Order(){
-
-    }
+    
     public Order(String id, String deliveryTime) {
         this.id=id;
-        this.deliveryTime=deliveryTime;
+        this.deliveryTime=Integer.parseInt(deliveryTime);
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
@@ -19,5 +16,5 @@ public class Order {
         return id;
     }
 
-    public String getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {return deliveryTime;}
 }
