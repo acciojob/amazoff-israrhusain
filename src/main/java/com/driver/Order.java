539e3d9075ba051e4a61c69ee27ff7a651985c08
israr <israrhusain5892@gmail.com>
2023-01-14 11:58:29 +0530

    buliderrr

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,4 +1,7 @@
 package com.driver;
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
 public class Order {
 
     private String id;
@@ -6,8 +9,18 @@ public class Order {
     
     public Order(String id, String deliveryTime) {
         this.id=id;
-        this.deliveryTime=Integer.parseInt(deliveryTime);
-
+        deliveryTime = String.format("%02d",hours) + ":" + String.format("%02d", minutes);
+        String[] timef=deliveryTime.split(":");  
+        
+        int hour=Integer.parseInt(timef[0]);  
+        int minute=Integer.parseInt(timef[1]);  
+         
+        
+          
+        this.deliveryTime =minute + (60 * hour);  
+        
+        
+         
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
     }
